979a67a3f693f1ea0f3e175526290ad96a2d36eb
Sujin Choi <65237193+sujin-1013@users.noreply.github.com>
2024-06-19 16:28:55 +0900

    Time: 24 ms (9.24%), Space: 11.7 MB (6.99%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        
+        new_nums = []
+
+        for num in nums:
+            if num != val:
+                new_nums.append(num)
+
+        print(new_nums)
+        k = len(new_nums)
+
+        for i in range(k):
+            nums[i] = new_nums[i]
+
+        return k
