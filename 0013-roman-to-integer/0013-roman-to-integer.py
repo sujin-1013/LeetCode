d198c29a3f90e53a7412caa54f99e24327914a75
Sujin Choi <65237193+sujin-1013@users.noreply.github.com>
2025-01-16 17:34:16 +0900

    Time: 2 ms (84.69%), Space: 18 MB (10.6%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,32 +1,16 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        
-        dic_symbol = {"I":1,
-        "V":5,
-        "X":10,
-        "L":50,
-        "C":100,
-        "D":500,
-        "M":1000
-        }
+        dic_symbol = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         number = 0
-        check = 0
-        for i in range(len(s)-1):
-            now_s = s[i]
-            next_s = s[i+1]
-            if dic_symbol[now_s] < dic_symbol[next_s]:
-                check = 1
-                not_sum = dic_symbol[next_s] - dic_symbol[now_s]
+        
+        for i in range(len(s) - 1):
+            # 다음 값이 더 크면 뺄셈, 아니면 덧셈
+            if dic_symbol[s[i]] < dic_symbol[s[i + 1]]:
+                number -= dic_symbol[s[i]]
             else:
-                if check == 1:
-                    number += not_sum
-                    check = 0
-                else:
-                    number += dic_symbol[now_s]
-            print(now_s, number)
-        if check == 1:
-            number += not_sum
-        else:
-            number += dic_symbol[s[-1]]
-
+                number += dic_symbol[s[i]]
+        
+        # 마지막 문자는 항상 더함
+        number += dic_symbol[s[-1]]
+        
         return number
