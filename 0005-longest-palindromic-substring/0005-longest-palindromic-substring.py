70b8b9d519b68155fc6bdebfdb890bb01956a13c
Sujin Choi <65237193+sujin-1013@users.noreply.github.com>
2025-03-05 16:03:19 +0900

    Time: 8366 ms (5.43%), Space: 17.7 MB (84.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,10 +6,7 @@ class Solution:
 
         for i in range(n):
             for j in range(n-1,i-1,-1):
-                # print(i,j)
                 new_s = s[i:j+1]
-                # print(new_s)
-                # print(new_s[::-1])
                 if new_s == new_s[::-1]:
                     if len(new_s) > len(answer):
                         answer = new_s
